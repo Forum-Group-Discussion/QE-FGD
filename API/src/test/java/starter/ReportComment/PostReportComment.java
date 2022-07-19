f1221fa8e1847fa1b9f311a215f1c522ce983f41
gladysprds <gladyscaprds@gmail.com>
2022-07-19 16:37:04 +0700

    Done API

--- API/src/test/java/starter/ReportComment/PostReportComment.java
+++ API/src/test/java/starter/ReportComment/PostReportComment.java
@@ -1,4 +1,4 @@
-package starter.ReportThread;
+package starter.ReportComment;
 
 import net.serenitybdd.rest.SerenityRest;
 import org.apache.commons.io.FileUtils;
@@ -12,36 +12,40 @@ import java.util.Map;
 
 import static org.hamcrest.Matchers.equalTo;
 
-public class PostReportThread {
+public class PostReportComment {
     private String token;
     String base_url = "http://44.206.244.111/v1/";
-    public String endpointReportThread(){
-        return base_url + "reportthread/";
+
+    public String endpointReportComment(){
+        return base_url + "reportcomment/";
     }
 
-    public void requestReportThread(int id_thread) throws IOException {
+    public void requestReportComment(int id_comment) throws IOException {
         this.token = FileUtils.readFileToString(new File(System.getProperty("user.dir") + "//src//test//resources//filejson//tokensForReporting.json"), StandardCharsets.UTF_8);
         JSONObject requestData = new JSONObject();
-        Map<String,Integer> thread = new HashMap<>();
-        thread.put("id", id_thread);
-        requestData.put("thread",thread);
+        Map<String,Integer> comment = new HashMap<>();
+        comment.put("id", id_comment);
+        requestData.put("comment",comment);
         requestData.put("report_type","This_thread_contains_inappropriate_and_Fraud_elements");
         SerenityRest.given().header("Content-Type", "application/json")
                 .header("Authorization", "Bearer " + this.token)
                 .body(requestData.toJSONString());
-        SerenityRest.when().post(endpointReportThread());
+        SerenityRest.when().post(endpointReportComment());
     }
-
     public void validateDataDetailAfterReport(String report_type, int id_thread,int user_id){
         SerenityRest.then().body("message",equalTo("SUCCESS"));
-        SerenityRest.then().body("data.thread.id",equalTo(id_thread));
-        SerenityRest.then().body("data.thread.users.id",equalTo(user_id));
+        SerenityRest.then().body("data.comment.id",equalTo(id_thread));
+        SerenityRest.then().body("data.comment.users.id",equalTo(user_id));
         SerenityRest.then().body("data.report_type",equalTo(report_type));
     }
-
     public void errorMessage(){
         SerenityRest.then().body("message",equalTo("DATA_NOT_FOUND"));
     }
+
+
+
+
+
 
 
 }
